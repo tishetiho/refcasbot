9a21440d70b291949af8167d8a112603316b5f13
tishetiho <moteoAZ@yandex.ru>
2026-04-04 21:11:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ ADMIN_ID = 5078764886
 CHANNELS = [
     {"id": -1003884251721, "url": "https://t.me/ludomove", "name": "ЛУДО ДВИЖ"},
     {"id": -1003674572550, "url": "https://t.me/banknotagifts", "name": "Pepe | NFT"}
+]
 KNB_TIMEOUT = 120  # 2 минуты на ход
 KNB_COMMISSION = 0.05 # 5% комиссия
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
